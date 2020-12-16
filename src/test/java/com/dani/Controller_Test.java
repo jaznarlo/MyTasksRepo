a1692e7fcc5f104e9635cd8f4841ff7d460e8698
dquintil <dquintil@everis.com>
2020-12-16 22:58:09 +0100

    A ver si ahora funciona

--- src/test/java/com/dani/Controller_Test.java
+++ src/test/java/com/dani/Controller_Test.java
@@ -1,25 +1,19 @@
 package com.dani;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
-
 import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -28,7 +22,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.dani.Entity.Task;
 import com.dani.dao.ITaskService;
-import com.dani.dao.TaskService;
 
 @SuppressWarnings("unused")
 
